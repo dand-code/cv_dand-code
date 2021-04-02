081ced7278a66beaf0513e14f26bb657df1455af
Dandara Almeida <dandara.avazquez@gmail.com>
2021-04-02 21:06:39 +0200

    update menu style

--- dand-code_cv/src/js/Menu.js
+++ dand-code_cv/src/js/Menu.js
@@ -2,28 +2,28 @@ import '../style/menu.scss';
 
 function Menu() {
 
-    const toggleSidebar =() =>{
+    const toggleSidebar = () => {
         const box = document.getElementById('sidebar');
         box.classList.toggle("active");
-         const btn = document.getElementById('sidebar-btn');
+        const btn = document.getElementById('sidebar-btn');
         btn.classList.toggle("toggle");
     }
-    
+
     return (
-            <div className="sidebar" id="sidebar">
-                <ul>
-                    <li><a href="https://codepen.io/jaflo/pen/hjuai">home</a></li>
-                    <li><a href="#aboutme">about</a></li>
-                    <li><a href="#education">education</a></li>
-                    <li><a href="#skills">skills</a></li>
-                    <li><a href="#experiences">work experiences</a></li>
-                    <li><a href="https://codepen.io/jaflo/pen/hjuai">contact</a></li>
-                </ul>
-                <button id="sidebar-btn" className="sidebar-btn" onClick={toggleSidebar}>
-                    <span></span>
-                </button>
-            </div>
-        
+        <div className="sidebar" id="sidebar">
+            <ul>
+                <li><a href="../../public/index.html">home</a></li>
+                <li><a href="#aboutme">about</a></li>
+                <li><a href="#education">education</a></li>
+                <li><a href="#skills">skills</a></li>
+                <li><a href="#experiences">work experiences</a></li>
+                <li><a href="https://codepen.io/jaflo/pen/hjuai">contact</a></li>
+            </ul>
+            <button id="sidebar-btn" className="sidebar-btn" onClick={toggleSidebar}>
+                <span></span>
+            </button>
+        </div>
+
     );
 }
 

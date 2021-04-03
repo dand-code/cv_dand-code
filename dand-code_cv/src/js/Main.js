446995008148913bcbc34ea48e4c859a99649c58
Dandara Almeida <dandara.avazquez@gmail.com>
2021-04-03 18:39:37 +0200

    fix background color hover main contact

--- dand-code_cv/src/js/Main.js
+++ dand-code_cv/src/js/Main.js
@@ -8,9 +8,9 @@ function Main() {
             <h3 className="main_contact">
                 <span className="main_contact">
                     <span className="bars"></span>  HTML, CSS, Javascript and React
-            </span>
+                </span>
                 <span className="main_contact">
-                    <a className="style-none" href="mailto:dandara.avazquez@gmail.com">&#8260;&#8260; dandara.avazquez@gmail.com</a>
+                    &#8260;&#8260; <a className="style-none" href="mailto:dandara.avazquez@gmail.com"> dandara.avazquez@gmail.com</a>
                 </span>
                 <span className="main_contact">
                     &#8260;&#8260; <a className="style-none" href="https://github.com/dand-code"> Github</a>

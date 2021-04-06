f0877bb752c0ccb9835e0054348cdb63b25efe3b
Dandara Almeida <dandara.avazquez@gmail.com>
2021-04-06 17:01:31 +0200

    update pdf file

--- src/js/Projects.js
+++ src/js/Projects.js
@@ -6,7 +6,7 @@ function Projects() {
       <h2 className="subtitle">projects</h2>
       <ul className="list">
         <li>
-          <h4>&#8260;&#8260; Searcher Ricky And Morty <a className="link" href="https://github.com/dand-code/search_ricky-morty">project</a> <a className="link" href="https://beta.adalab.es/modulo-3-evaluacion-final-dand-code/#/">web</a></h4>
+          <h4>&#8260;&#8260; Searcher Rick And Morty <a className="link" href="https://github.com/dand-code/search_ricky-morty">project</a> <a className="link" href="https://beta.adalab.es/modulo-3-evaluacion-final-dand-code/#/">web</a></h4>
         </li>
         <li>
           <h4>&#8260;&#8260; Guess The Number - Game <a className="link" href="https://github.com/dand-code/Guess-the-Number---Game">project</a> <a className="link" href="https://dand-code.github.io/Guess-the-Number---Game/">web</a></h4>
